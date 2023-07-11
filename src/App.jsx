2147a7f521101cc35b938640e7f6655d226fe83c
MomboteQ <momboteq@gmail.com>
2023-07-11 18:53:34 +0200

    Fixed react.yml, App.jsx and vite.config.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 function App() {
-    const url = import.meta.env.BASE_URL;
-
     return (
         <main className="p-[1.5rem] flex flex-col justify-center items-center min-h-[100dvh]">
             <div className="bg-cardBg p-[1.5rem] rounded-[.9rem] w-full max-w-[21.5rem]">
                 <div className="relative cursor-pointer">
-                    <img src={`${url}images/image-equilibrium.jpg`} alt="equilibrium NFT" className="rounded-[.5rem]" />
+                    <img src="/images/image-equilibrium.jpg" alt="equilibrium NFT" className="rounded-[.5rem]" />
                     <div className="absolute top-0 left-0 w-full h-full flex justify-center items-center bg-cyan bg-opacity-50 rounded-[.5rem] opacity-0 transition-opacity duration-300 hover:opacity-100">
-                        <img src={`${url}images/icon-view.svg`} alt="eye icon" />
+                        <img src="/images/icon-view.svg" alt="eye icon" />
                     </div>
                 </div>
                 <div className="flex flex-col gap-y-[1rem]">
@@ -15,17 +13,17 @@ function App() {
                     <p className="text-gray font-light">Our Equilibrium collection promotes balance and calm.</p>
                     <div className="flex justify-between items-center">
                         <span className="flex text-cyan font-medium">
-                            <img src={`${url}images/icon-ethereum.svg`} alt="ethereum icon" className="mr-[.4rem]" /> 0.041 ETH
+                            <img src="/images/icon-ethereum.svg" alt="ethereum icon" className="mr-[.4rem]" /> 0.041 ETH
                         </span>
                         <span className="flex text-gray font-light">
-                            <img src={`${url}images/icon-clock.svg`} alt="clock icon" className="mr-[.4rem]" /> 3 days left
+                            <img src="/images/icon-clock.svg" alt="clock icon" className="mr-[.4rem]" /> 3 days left
                         </span>
                     </div>
                 </div>
                 <div className="w-full h-[.065rem] bg-line my-[.9rem]"></div>
                 <div className="flex mt-[.2rem] items-center">
                     <img
-                        src={`${url}images/image-avatar.png`}
+                        src="/images/image-avatar.png"
                         alt="creator avatar"
                         className="w-[2rem] border-white border-[.065rem] rounded-full mr-[1rem]"
                     />
